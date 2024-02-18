76d365f0716313eb33411369c2078bb0c56467e9
Suchan Kim <somnium421@kaist.ac.kr>
2024-02-18 20:51:04 +0900

    HomeTag

--- nal/src/HomePhoto.tsx
+++ nal/src/HomePhoto.tsx
@@ -1,10 +1,24 @@
 import './HomePhoto.css'
 import './font.css'
 
+interface Props {
+    top: string;
+    left: string;
+}
+
+const homeTag = (props: Props) => {
+    return (
+        <div id="homeTag" style={{top: props.top, left:props.left}}>
+            <div id="homeTagTri"></div>
+            <div id="homeTagBox">Myeondong, Seoul</div>
+        </div>
+    )
+}
+
 const HomePhoto = () => {
     return (
         <div id="homePhoto">
-            {/* <img id="exampleImg1" src="img/Home.jpg" alt="" /> */}
+            {/* <img id="exampleImg1" src="img/HOME_WITH_TAGS.jpg" alt="" /> */}
             <img id="exampleImg" src="img/DSCF9377.jpg" alt=""  onClick={() => {
                 console.log("Image Clicked");
             }}/>
@@ -13,7 +27,7 @@ const HomePhoto = () => {
                 <div id="homePhotoTextBottom"><span id="weatherFelt">Weather felt</span> cold.</div>
             </div>
             <div id="homeTags">
-                
+                {homeTag({top: '4.2vh', left: '18.5vh'})}                
             </div>
         </div>
     );
