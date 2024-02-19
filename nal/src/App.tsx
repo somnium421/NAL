164da2ea836d0eb886c1715053c000744c6f0607
Suchan Kim <somnium421@kaist.ac.kr>
2024-02-19 10:48:08 +0900

    basic scroll added

--- nal/src/App.tsx
+++ nal/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 import './font.css';
 import StatusBar from './StatusBar';
--- nal/src/HomeScroll.tsx
+++ nal/src/HomeScroll.tsx
@@ -1,12 +1,35 @@
-import './HomeScroll.css';
+import { useEffect, useRef } from 'react'
+import './HomeScroll.css'
 import Weather from './Weather'
 import UpcomingEvents from './UpcomingEvents'
 
 const HomeScroll = () => {
+    const scrollRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const scrollRefCurrent = scrollRef.current;
+        const wheelHandler = (e: Event) => {
+            e.preventDefault();
+            if (scrollRefCurrent instanceof HTMLDivElement) {
+                console.log(e);                
+            }
+        }
+        if (scrollRefCurrent instanceof HTMLDivElement) {
+            scrollRefCurrent.addEventListener("wheel", wheelHandler);
+        }
+        return () => {
+            if (scrollRefCurrent instanceof HTMLDivElement) {
+            scrollRefCurrent?.removeEventListener("wheel", wheelHandler);
+            }
+        }
+    }, [])
+
     return (
-        <div id="homeScroll">
-            <Weather/>
-            <UpcomingEvents/>
+        <div id="scroll">
+            <div id="homeScroll">
+                <Weather/>
+                <UpcomingEvents/>
+            </div>
         </div>
     );
 }
